f4d08fafcc07a95fd3ee22dff2aacba07a0fa9cf
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 23:08:33 +0530

    Added: Mood Analyser UC-2(Test Case)

--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,15 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in SAD Mood.");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("SAD", mood);
-    }
-
-    @Test
-    public void givenMessage_WhenHappy_ShouldReturnHappy(){
-        MoodAnalyser moodAnalyser =  new MoodAnalyser("I am in Any Mood.");
+    public void givenNullMood_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
         String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("HAPPY", mood);
     }
