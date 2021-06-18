cd629433bff8599252c8dbc4092d940407ff0c9e
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 22:44:06 +0530

    Added: Mood Analyser UC-1

--- main/java/com/moodanalyser/MoodAnalyser.java
+++ main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,7 +1,11 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
-    public static void main(String[] args){
-        System.out.println("Welcome to Mood Analyser Program");
+
+    public String analyseMood(String message) {
+        if(message.contains("SAD"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
