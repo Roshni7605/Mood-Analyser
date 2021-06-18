4bbeb3a8b996eb6a8ce66546e2ad06f2755dc759
Roshni1998 <roshnimali847@gmail.com>
2021-06-18 22:53:32 +0530

    Added: Refactor Mood Analyser UC-1(Test Case)

--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in SAD Mood.");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in SAD Mood.");
+        String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("SAD", mood);
     }
 
     @Test
     public void givenMessage_WhenHappy_ShouldReturnHappy(){
-        MoodAnalyser moodAnalyser =  new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Any Mood.");
+        MoodAnalyser moodAnalyser =  new MoodAnalyser("I am in Any Mood.");
+        String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("HAPPY", mood);
     }
 }
