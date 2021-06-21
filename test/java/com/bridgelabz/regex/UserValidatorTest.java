796de80aee0965cb0b7d1eae093d90600cbbf8a0
Roshni1998 <roshnimali847@gmail.com>
2021-06-21 20:41:23 +0530

    Added: User Registration UC-9 (Test Case)

---
+++ test/java/com/bridgelabz/regex/UserValidatorTest.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.regex;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserValidatorTest {
+
+        @Test
+        public void givenEmailId_WhenProper_ShouldReturnTrue(){
+            com.userregistration.UserValidator valid = new com.userregistration.UserValidator();
+            assertTrue(valid.isValidEmailId("abc@yahoo.com"));
+            assertTrue(valid.isValidEmailId("abc-100@yahoo.com"));
+            assertTrue(valid.isValidEmailId("abc.100@yahoo.com"));
+            assertTrue(valid.isValidEmailId("abc111@abc.com"));
+            assertTrue(valid.isValidEmailId("abc-100@abc.net"));
+            assertTrue(valid.isValidEmailId("abc.100@abc.com.au"));
+            assertTrue(valid.isValidEmailId("abc@1.com"));
+            assertTrue(valid.isValidEmailId("abc@gmail.com.com"));
+            assertTrue(valid.isValidEmailId("abc+100@gmail.com"));
+        }
+
+        @Test
+        public void givenEmailId_WhenNotProper_ShouldReturnFalse(){
+            UserValidator valid = new UserValidator();
+            assertFalse(valid.isValidEmailId("abc.@gmail.com"));
+            assertFalse(valid.isValidEmailId("abc@.com.my"));
+            assertFalse(valid.isValidEmailId("abc123@gmail.a"));
+            assertFalse(valid.isValidEmailId("abc123@.com"));
+            assertFalse(valid.isValidEmailId("abc123@.com.com"));
+            assertFalse(valid.isValidEmailId(".abc@abc.com"));
+            assertFalse(valid.isValidEmailId("abc()*@gmail.com"));
+            assertFalse(valid.isValidEmailId("abc@%*.com"));
+            assertFalse(valid.isValidEmailId("abc..2002@gmail.com"));
+            assertFalse(valid.isValidEmailId("abc.@gmail.com"));
+            assertFalse(valid.isValidEmailId("abc@gmail.com.1a"));
+            assertFalse(valid.isValidEmailId("abc@gmail.com.aa.au"));
+        }
+}
